4a746ac5afb07929671bf261db16e8dab935e965
SteveFWagner <sfwagner1011@gmail.com>
2019-01-29 17:02:50 -0700

    Finished required +

--- practice.js
+++ practice.js
@@ -18,7 +18,9 @@ var arr = [10,20,30];
 */
 
 //Code Here
-
+function first(arr){
+  return arr[0]
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -33,7 +35,9 @@ var arr = [40,50,60];
 */
 
 //Code Here
-
+function last(b){
+  return b[b.length-1]
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -48,7 +52,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 */
 
 //Code Here
-
+function looper(fam){
+  for(let i = 0;i<fam.length;i++){
+    alert(fam[i]);
+  }
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -63,7 +71,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
-
+function reversedLooper(letters){
+  for(let i = letters.length-1;i>=0 ;i--){
+    alert(letters[i]);
+  }
+}
 
 
 ////////// PROBLEM 5 //////////
@@ -79,7 +91,15 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 
 //Code Here
 
-
+function evenFinder(n){
+  let evens =[];
+  for(let i=0;i<n.length;i++){
+    if(n[i]%2===0){
+      evens.push(n[i]);
+    }
+  }
+  return evens
+}
 
 
 
@@ -106,7 +126,21 @@ var numbersArray = [1,2,34,54,55,34,32,11,19,17,54,66,13];
 */
 
 //Code Here
-
+function divider(x){
+  var evenNums =[];
+  var oddNums =[];
+  var beastMode =[];
+  for(let i = 0;i < x.length;i++){
+    if(x[i]%2===0){
+      evenNums.push(x[i])
+    }else if(x[i]%2===1){
+      oddNums.push(x[i])
+    }
+  }
+beastMode.push(evenNums)
+beastMode.push(oddNums)
+return beastMode
+}
 
 
 ////////// PROBLEM 7 //////////
@@ -128,6 +162,11 @@ var getRandomArbitrary = function() {
 
 //Code Here
 
+
+function finder(k){
+  let answer = k.indexOf(getRandomArbitrary()) !== -1? true:false
+  return answer
+}
 
 
 ////////// PROBLEM 8 //////////
@@ -156,7 +195,22 @@ var myGroceryList = ['chips', 'pizza', 'hotpockets', 'MtnDew', 'corndogs'];
 */
 
 //Code Here
-
+function removeItem(list,item){
+  if(list && item){
+    if(list.indexOf(item) !== -1){
+      list.splice(list.indexOf(item),1)
+      return list
+    } 
+    else {
+      return list
+    }
+  } 
+  else {
+    
+    return [];
+  }
+  
+}
 
 
 ////////// PROBLEM 9 //////////
